4625935459d802e8861560723091b0168c02c6aa
IsraelCG <israelcarrion.g@gmail.com>
2023-10-12 23:12:27 -0300

    sprint-4

---
+++ src/main/resources/static/Web/Scripts/account.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      id:"",
+      account: [],
+    };
+  },
+  created(){
+    this.id =  new URLSearchParams(location.search).get("id");
+    this.loadAccount(this.id);
+  },
+  methods:{
+    loadAccount(id){
+        axios.get(`/api/accounts/${id}`)
+        .then(({data}) => {
+            this.account = data;
+        })
+        .catch(err => console.log(err))
+    },
+    dateFormat(date) {
+      return moment(date).format('lll');
+    }
+
+  }
+}).mount("#app");
